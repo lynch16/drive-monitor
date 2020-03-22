929c92231b19c1425fe51e052762f8a1789b611c
Will Lynch <will.lynch91@gmail.com>
2020-03-22 16:29:33 -0400

    change to one monitor at a time

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 const { Monitor } = require("./lib/monitor");
 const day = 24 * 60 * 60 * 1000;
-const interval = 7;
+const interval = process.env.INTERVAL || 7;
 const timeframe = interval * day;
 
-Promise.all([
-  {
-    id: process.env.HOME,
-    message: `The camera has not recorded data in ${interval} days. Should you check that its working properly?`
-  }, 
-  // {
-  //   id: process.env.COMPUTER_ROOM_ID,
-  //   message: `The camera in the Computer Room has not recorded data in ${interval} days. Should you check that its working properly?`
-  // }, 
-  // // {
-  //   id: process.env.ELECTRONICS_ROOM_ID,
-  //   message: `The camera in the Electronics Room has not recorded data in ${interval} days. Should you check that its working properly?`
-  // }, 
-  // {
-  //   id: process.env.LOCKER_ROOM_ID,
-  //   message: `The camera in the Locker Room has not recorded data in ${interval} days. Should you check that its working properly?`
-  // }
-].map(
-  ({ id, message }) => new Monitor(
-    id,
-    {
-      channel: "GTU6HUUUW",
-      message,
-    },
-    timeframe,
-  ).monitor()
-));
+const message = `The${process.env.NAME ? ` ${process.env.NAME}` : ""} camera has not recorded data in ${interval} days. Should you check that its working properly?`
 
+new Monitor(
+  process.env.FOLDER_ID,
+  {
+    message,
+    channel: process.env.CHANNEL_ID,
+  },
+  timeframe
+).monitor();
